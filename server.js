c1951e29f22337c228a7755d7f26da2ff45b088a
Saurja Ghosh <saurjaghoshofficial@gmail.com>
2021-06-20 15:37:30 +0530

    Router Setup

--- server.js
+++ server.js
@@ -1,37 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// ####### Router Modules
+const bootcamps = require("./routes/bootcamps");
+
 // ####### LOAD ENV VARIABLES
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-// ####### Routes
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show All BootCamps" });
-});
-
-app.get("/api/v1/bootcamp/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Display bootcamp ${req.params.id}` });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create new BootCamp" });
-});
-
-app.put("/api/v1/bootcamp/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamp/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+// ####### Mount Router
+app.use("/api/v1/bootcamps", bootcamps);
 
 // ####### Server Runtime
 const PORT = process.env.PORT || 5000;
